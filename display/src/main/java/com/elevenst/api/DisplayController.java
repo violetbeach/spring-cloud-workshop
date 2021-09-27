c8b2b44b07b9a5d018348d5b741943a9f4560ac1
Lee Jaeheon <ti6419@gmail.com>
2021-09-27 11:48:46 +0900

    feat: Add remote transfer

--- display/src/main/java/com/elevenst/api/DisplayController.java
+++ display/src/main/java/com/elevenst/api/DisplayController.java
@@ -5,13 +5,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.elevenst.service.ProductRemoteService;
+
 @RestController
 @RequestMapping(path = "/displays")
 public class DisplayController {
+	
+	private final ProductRemoteService productRemoteService;
 
-    @GetMapping(path = "/{displayId}")
+    public DisplayController(ProductRemoteService productRemoteService) {
+		this.productRemoteService = productRemoteService;
+	}
+
+	@GetMapping(path = "/{displayId}")
     public String getDisplayDetail(@PathVariable String displayId) {
-        String productInfo = "[unknown]";
+        String productInfo = productRemoteService.getProductInfo("1111");
         return String.format("[display id = %s at %s %s ]", displayId, System.currentTimeMillis(), productInfo);
     }
 }
